da3aa2c975bf8500fd37abf05d34a67194adce3e
EkaterinaAntipenko <ekaterina097a@gmail.com>
2024-12-24 14:29:46 +0300

    sadness

---
+++ docs/styleguide.221ca50028d0f2dd8450.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  window.onload = function () {
+    setTimeout(function () {
+      document.getElementById("preloader").style.opacity = "0";
+      document.getElementById("preloader").style.pointerEvents = "none";
+    }, 1200);
+  };
+});
+/******/ })()
+;
